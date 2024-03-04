df226b6d31034204a1fd91a4ff48041bc338a678
Rishirk2107 <rishikumarss2004@gmail.com>
2024-03-04 09:52:54 +0530

    Third Commit

--- index.js
+++ index.js
@@ -162,10 +162,10 @@ io.on('connection', (socket) => {
   });
 
 
-  socket.on('joinRoom', (room) => {
-    socket.join(room);
-    console.log(`User joined room ${room}`);
-  });
+  // socket.on('joinRoom', (room) => {
+  //   socket.join(room);
+  //   console.log(`User joined room ${room}`);
+  // });
 
   socket.on('sendMessage', (room, message) => {
     io.to(room).emit('message', message);
